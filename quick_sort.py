cc8b27478da6cb9219927a00644ce263960aacdf
Procopii Maria <77497709+MariaProcopii@users.noreply.github.com>
2023-02-19 23:45:45 +0200

    Create quick_sort.py

---
+++ quick_sort.py
@@ -0,0 +1,8 @@
+def quick_sort(array):
+    if len(array) <= 1:
+        return array
+    else:
+        pivot = array[0]
+        left = [x for x in array[1:] if x <= pivot]
+        right = [x for x in array[1:] if x > pivot]
+        return quick_sort(left) + [pivot] + quick_sort(right)
