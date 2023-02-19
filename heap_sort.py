e4d9454f670461946db2e35818ff0be72f7c649a
Procopii Maria <77497709+MariaProcopii@users.noreply.github.com>
2023-02-20 00:21:05 +0200

    Create heap_sort.py

---
+++ heap_sort.py
@@ -0,0 +1,27 @@
+def heap_sort(arr):
+    n = len(arr)
+
+    # Build a max heap from the input array
+    for i in range(n // 2 - 1, -1, -1):
+        heapify(arr, n, i)
+
+    # Extract elements from the heap one by one
+    for i in range(n - 1, 0, -1):
+        arr[0], arr[i] = arr[i], arr[0]
+        heapify(arr, i, 0)
+
+def heapify(arr, n, i):
+    # Given a subtree rooted at i, make sure that it satisfies the max heap property
+    largest = i
+    left = 2 * i + 1
+    right = 2 * i + 2
+
+    if left < n and arr[left] > arr[largest]:
+        largest = left
+
+    if right < n and arr[right] > arr[largest]:
+        largest = right
+
+    if largest != i:
+        arr[i], arr[largest] = arr[largest], arr[i]
+        heapify(arr, n, largest)
