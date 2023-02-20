0c8e53b6b840c6c8e40d75386b1bada4f516fa49
Procopii Maria <77497709+MariaProcopii@users.noreply.github.com>
2023-02-20 10:52:03 +0200

    Update quick_sort.py

--- quick_sort.py
+++ quick_sort.py
@@ -1,8 +1,18 @@
-def quick_sort(array):
-    if len(array) <= 1:
-        return array
-    else:
-        pivot = array[0]
-        left = [x for x in array[1:] if x <= pivot]
-        right = [x for x in array[1:] if x > pivot]
-        return quick_sort(left) + [pivot] + quick_sort(right)
+def quick_sort(arr):
+    # If the array has length 0 or 1, it is already sorted
+    if len(arr) <= 1:
+        return arr
+
+    # Choose the first element as the pivot
+    pivot = arr[0]
+
+    # Partition the array into two sub-arrays
+    left = [x for x in arr[1:] if x < pivot]
+    right = [x for x in arr[1:] if x >= pivot]
+
+    # Recursively sort the sub-arrays
+    left_sorted = quick_sort(left)
+    right_sorted = quick_sort(right)
+
+    # Concatenate the sorted sub-arrays with the pivot and return the result
+    return left_sorted + [pivot] + right_sorted
